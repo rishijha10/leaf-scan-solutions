5c2e13b6af221ec72e4cc43b82e6802c51a48751
Rishi Jha <1032200587@tcetmumbai.in>
2024-01-30 14:33:43 +0530

    backend folder added

---
+++ backend/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify
+import tensorflow as tf
+from PIL import Image
+import numpy as np
+from flask_cors import CORS  # Import CORS from flask_cors
+
+app = Flask(__name__)
+CORS(app)
+
+# app = Flask(__name__)
+
+
+
+# Load the TensorFlow SavedModel
+model_path = "model"  # Replace with your model's directory path
+model = tf.saved_model.load(model_path)
+
+# Replace this list with your actual array of class labels
+class_labels = ['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy', 'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew', 'Cherry_(including_sour)___healthy', 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 'Corn_(maize)___Common_rust_', 'Corn_(maize)___Northern_Leaf_Blight', 'Corn_(maize)___healthy', 'Grape___Black_rot', 'Grape___Esca_(Black_Measles)', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Peach___Bacterial_spot', 'Peach___healthy', 'Pepper,_bell___Bacterial_spot', 'Pepper,_bell___healthy', 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 'Raspberry___healthy', 'Soybean___healthy', 'Squash___Powdery_mildew', 'Strawberry___Leaf_scorch', 'Strawberry___healthy', 'Tomato___Bacterial_spot', 'Tomato___Early_blight', 'Tomato___Late_blight', 'Tomato___Leaf_Mold', 'Tomato___Septoria_leaf_spot', 'Tomato___Spider_mites Two-spotted_spider_mite', 'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus', 'Tomato___healthy']  # Your list of class labels
+
+def preprocess_image(image):
+    img = Image.open(image)
+    img = img.resize((224, 224))
+    img_array = np.array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+    return img_array
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'})
+
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'})
+
+    if file:
+        img_array = preprocess_image(file)
+        input_tensor = tf.convert_to_tensor(img_array, dtype=tf.float32)
+        prediction = model(input_tensor)
+        predicted_class = np.argmax(prediction.numpy())
+        predicted_label = class_labels[predicted_class]
+        return jsonify({'predicted_disease': predicted_label})
+
+if __name__ == '__main__':
+    app.run(debug=True)  # Run the Flask app
